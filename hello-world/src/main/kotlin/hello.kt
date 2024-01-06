420e7e7aed3be633b415c99526e614f0e8f14509
FariasGabriela <gabrielafarias@alunos.utfpr.edu.br>
2024-01-06 18:23:15 -0300

    Criação de projeto, log e variaveis

---
+++ hello-world/src/main/kotlin/hello.kt
@@ -0,0 +1,15 @@
+fun main(args : Array<String>) {
+    //COMO FAZER UM LOG
+    println("Deu certo, primeira log deu certo!")
+
+    //Criar uma variavel em Kotlin
+    val primeiraVariavel: String = "Oi, a primeira variável deu boa"
+
+    println(primeiraVariavel)
+
+    //Variavel do tipo numero
+    val primeiroInteiro: Int = 1000
+
+    println("Primeiro string e variável: $primeiraVariavel")
+    println("Primeiro número: $primeiroInteiro")
+}
