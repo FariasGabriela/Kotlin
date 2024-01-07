6d9b9617d7377810f0963e6f7395bc746ef522e6
FariasGabriela <gabrielafarias@alunos.utfpr.edu.br>
2024-01-07 16:32:33 -0300

    estudos com função lambda em kotlin

--- hello-world/src/main/kotlin/funcoes.kt
+++ hello-world/src/main/kotlin/funcoes.kt
@@ -1,6 +1,7 @@
 fun main(args : Array<String>) {
     firstFunction()
     println(secondFunction())
+    println(funcaoLambda())
 }
 
 fun firstFunction() {
@@ -9,4 +10,9 @@ fun firstFunction() {
 
 fun secondFunction() : String {
     return "chamando segunda função"
+}
+
+fun funcaoLambda() : Int {
+    val list = listOf<Int>(1, 5, 7, 8, 9, 10)
+    return list.count { n -> n > 6 }
 }
